98118a368c5abfa3ae854464ee4f840e052403a5
sbarry <bsidy01@gmail.com>
2021-10-26 11:26:22 +0200

    commit of my "projet de stage "  26/10/2021   12h 25

---
+++ src/app/create-etablissement/create-etablissement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cf-create-etablissement',
+  templateUrl: './create-etablissement.component.html',
+  styleUrls: ['./create-etablissement.component.css']
+})
+export class CreateEtablissementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
